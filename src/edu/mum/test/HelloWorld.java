04ccc847fc1a27564b16bf85a21e36bd853dc707
renukamohanraj <rmohanraj@mum.edu>
2015-04-09 17:31:06 -0500

    First Program

---
+++ src/edu/mum/test/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.mum.test;
+
+/**
+ * @author rMohanraj
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+	}
+
+}
